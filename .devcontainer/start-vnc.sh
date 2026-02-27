0589ba23fae468c066e585fee6e88bee47064890
UmarAbubacar <umarfaruk65@gmail.com>
2026-02-27 23:18:20 +0000

    fix3

--- .devcontainer/start-vnc.sh
+++ .devcontainer/start-vnc.sh
@@ -6,25 +6,30 @@ mkdir -p "$XDG_RUNTIME_DIR" && chmod 700 "$XDG_RUNTIME_DIR"
 # Kill any existing processes
 killall Xvfb x11vnc fluxbox websockify 2>/dev/null || true
 rm -f /tmp/.X1-lock /tmp/.X11-unix/X1 2>/dev/null
+rm -f /tmp/xvfb.pid /tmp/x11vnc.pid /tmp/fluxbox.pid /tmp/websockify.pid
 sleep 1
 
-# Start Xvfb virtual framebuffer (setsid to survive SIGHUP)
-setsid Xvfb :1 -screen 0 1920x1080x24 > /tmp/xvfb.log 2>&1 &
+# Start Xvfb virtual framebuffer
+start-stop-daemon --start --background --pidfile /tmp/xvfb.pid --make-pidfile \
+  --exec /usr/bin/Xvfb -- :1 -screen 0 1920x1080x24
 sleep 2
 
 # Start window manager
-setsid fluxbox > /tmp/fluxbox.log 2>&1 &
+start-stop-daemon --start --background --pidfile /tmp/fluxbox.pid --make-pidfile \
+  --exec /usr/bin/fluxbox
 sleep 1
 
-# Start x11vnc to mirror the Xvfb display over VNC (port 5900)
-setsid x11vnc -display :1 -forever -nopw -rfbport 5900 -shared > /tmp/x11vnc.log 2>&1 &
+# Start x11vnc
+start-stop-daemon --start --background --pidfile /tmp/x11vnc.pid --make-pidfile \
+  --exec /usr/bin/x11vnc -- -display :1 -forever -nopw -rfbport 5900 -shared
 sleep 1
 
 # Symlink for noVNC landing page
 ln -sf /usr/share/novnc/vnc.html /usr/share/novnc/index.html 2>/dev/null
 
-# Start websockify (setsid to survive SIGHUP)
-setsid websockify --web=/usr/share/novnc 6080 localhost:5900 > /tmp/websockify.log 2>&1 &
+# Start websockify
+start-stop-daemon --start --background --pidfile /tmp/websockify.pid --make-pidfile \
+  --startas /usr/bin/websockify -- --web=/usr/share/novnc 6080 localhost:5900
 
 # Wait for port 6080 to be ready
 for i in $(seq 1 30); do
